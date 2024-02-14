ac49faecfb266ff1bdfdd26d4921091b8b76287e
BadisBoughanmi <badisboughanmi555@gmail.com>
2024-02-14 14:27:41 +0100

    nav done

--- front2/myapp/src/components/Navbar.jsx
+++ front2/myapp/src/components/Navbar.jsx
@@ -8,35 +8,34 @@ import { IoEnter } from "react-icons/io5";
 
 import './nav.css'
 export default function Navbar() {
-    const [showSignUp, setShowSignUp] = useState(false);
-    const [showSignIn, setShowSignIn] = useState(false);
-  
-    const toggleSignUp = () => {
-      setShowSignUp(!showSignUp);
-      setShowSignIn(false); // Close signin form if it's open
-    };
-  
-    const toggleSignIn = () => {
-      setShowSignIn(!showSignIn);
-      setShowSignUp(false); // Close signup form if it's open
-    };
-  
-    return (
-        <nav className='nav-cont'>
-          <Link href="/" className="home-link">Home</Link>
-          <div className="nav-buttons">
-            <ul>
-              <li>
-                <IoEnter onClick={toggleSignUp} size={35}/>
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [showSignIn, setShowSignIn] = useState(false);
 
-              </li>
-              <li>
-                <MdPerson onClick={toggleSignIn} size={35}/>
-              </li>
-            </ul>
-          </div>
-          {showSignUp && <SignUp />}
-          {showSignIn && <SignIn />}
-        </nav>
-      );
-    }
+  const toggleSignUp = () => {
+    setShowSignUp(!showSignUp);
+    setShowSignIn(false); // Close signin form if it's open
+  };
+
+  const toggleSignIn = () => {
+    setShowSignIn(!showSignIn);
+    setShowSignUp(false); // Close signup form if it's open
+  };
+
+  return (
+      <nav className='nav-cont'>
+        <Link href="/" className="home-link">Home</Link>
+        <div className="nav-buttons">
+          <ul>
+            <li>
+              <IoEnter onClick={toggleSignUp} size={35}/>
+            </li>
+            <li>
+              <MdPerson onClick={toggleSignIn} size={35}/>
+            </li>
+          </ul>
+        </div>
+        {showSignUp && <SignUp />}
+        {showSignIn && <SignIn />}
+      </nav>
+    );
+  }
