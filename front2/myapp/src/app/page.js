9fab18063e1db696c1af9b901fdb07ce630a2c9b
Ghada000 <assidighada4@gmail.com>
2024-02-15 15:14:47 +0100

    dd

--- front2/myapp/src/app/page.js
+++ front2/myapp/src/app/page.js
@@ -1,13 +1,16 @@
-"use client"
 import Image from "next/image";
 import Link from "next/link";
-// import Navbar from "../components/Navbar.jsx";
-import About from "../pages/About/index.js"
-import NavbarServices from  "../components/NavBarServices.jsx"
-export default function Home() {
+import Navbar from "../components/Navbar.jsx";
+import About from "../pages/About/about.js"
+import NavbarGoods from "@/components/navbarGoods.jsx";
+// import Home from "@/pages/home/home.jsx";
+export default function test() {
   return (
-    <div>
-   <NavbarServices/>
-   </div>
+    <>
+   {/* <Home/> */}
+   <Navbar/>
+   <About/>
+
+    </>
   );
 }
