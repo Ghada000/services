a392911e82d08a1768a56a811e875ae6169cd219
Ghada000 <assidighada4@gmail.com>
2024-02-15 10:35:51 +0100

    v

--- backend/controllers/servicesControllers.js
+++ backend/controllers/servicesControllers.js
@@ -56,10 +56,33 @@ const deleted = function(req, res) {
   });
 };
 
+const getComments = function(req, res) {
+  const { service_id } = req.params;
+  getAllComments(service_id, function(err, results) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(results);
+    }
+  });
+};
+
+const postComment = function(req, res) {
+  const { service_id } = req.params;
+  const { comment_text } = req.body;
+  postComment(service_id, comment_text, function(err, result) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).json(result);
+    }
+  });
+};
+
 module.exports = {
   get,
   getByServiceType,
   posted,
   put,
-  deleted
+  deleted, getComments, postComment
 };
